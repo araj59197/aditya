970f37c20de54196ef2ed60d67d5822d9d450856
araj59197 <90503169+araj59197@users.noreply.github.com>
2022-05-12 00:52:55 +0530

    Create pattern1.cpp

---
+++ pattern1.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //       1 1 1 1 1
+    //       1 2 2 2 1
+    //       1 2 3 2 1
+    //       1 2 2 2 1
+    //       1 1 1 1 1
+    
+    int x;
+    cin >> x;
+    int p = x;
+    if (x % 2 != 0)
+    {
+        int i = 1;
+        while (p >= 1)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                if (j == x - 1 - ((x / 2) - 1))
+                    continue;
+                cout << j << "\t";
+            }
+            for (int j = p - 2; j > 0; j--)
+            {
+                cout << i << "\t";
+            }
+            for (int j = i; j > 0; j--)
+            {
+                cout << j << "\t";
+            }
+            cout << endl;
+            p = p - 2;
+            i = i + 1;
+        }
+        p = p + 4;
+        while (p != x + 2)
+        {
+            for (int j = 1; j < i - 2; j++)
+            {
+                cout << j << "\t";
+            }
+            for (int j = p - 1; j > 0; j--)
+            {
+                cout << i - 2 << "\t";
+            }
+            for (int j = i - 2; j > 0; j--)
+            {
+                cout << j << "\t";
+            }
+            p = p + 2;
+            i = i - 1;
+            cout << endl;
+        }
+    }
+    else
+    {
+        int i = 1;
+        while (p >= 1)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+
+                cout << j << "\t";
+            }
+            for (int j = p - 2; j > 0; j--)
+            {
+                cout << i << "\t";
+            }
+            for (int j = i; j > 0; j--)
+            {
+                cout << j << "\t";
+            }
+            cout << endl;
+            p = p - 2;
+            i = i + 1;
+        }
+        p = p + 2;
+        i = i - 1;
+        for (int j = 1; j < i; j++)
+            cout << j << "\t";
+        for (int j = p; j > 0; j--)
+        {
+            cout << i << "\t";
+        }
+        for (int j = i - 1; j > 0; j--)
+            cout << j << "\t";
+        cout << endl;
+        i = i + 1;
+        p = p + 2;
+
+        while (p != x + 2)
+        {
+            for (int j = 1; j < i - 2; j++)
+            {
+                cout << j << "\t";
+            }
+            for (int j = p - 1; j > 0; j--)
+            {
+                cout << i - 2 << "\t";
+            }
+            for (int j = i - 2; j > 0; j--)
+            {
+                cout << j << "\t";
+            }
+            p = p + 2;
+            i = i - 1;
+            cout << endl;
+        }
+    }
+    return 0;
+}
